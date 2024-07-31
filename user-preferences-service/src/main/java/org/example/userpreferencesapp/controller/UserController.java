33cc0dab5f53a95954ce3df4daa8a531ec2e7d93
guy4213 <113546078+guy4213@users.noreply.github.com>
2024-07-31 19:28:24 +0300

    Update UserController.java
    adding delete user by id method.

--- user-preferences-service/src/main/java/org/example/userpreferencesapp/controller/UserController.java
+++ user-preferences-service/src/main/java/org/example/userpreferencesapp/controller/UserController.java
@@ -3,6 +3,8 @@ package org.example.userpreferencesapp.controller;
 import io.dapr.client.DaprClient;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import org.example.userpreferencesapp.dto.user.DeleteUserResponseDto;
 import org.example.userpreferencesapp.dto.user.UserListDto;
 import org.example.userpreferencesapp.dto.user.UserRegistrationRequest;
 import org.example.userpreferencesapp.dto.user.UserResponse;
@@ -31,9 +33,7 @@ public class UserController {
     @PostMapping("/register")
     public Mono<ResponseEntity<Object>> registerUser(@Valid @RequestBody UserRegistrationRequest request,
                                                      @RequestParam(value = "preferencesNames", required = true) String... preferencesNames) {
-        if (preferencesNames.length > 5) {
-            return Mono.just(ResponseEntity.badRequest().body("Please enter maximum 5 preferences."));
-        }
+     
         Map<String, String> metadata = new HashMap<>();
         metadata.put("cloudevent.datacontenttype", "application/*+json");
         return Mono.fromCallable(() -> userService.registerUser(request, preferencesNames))
@@ -66,6 +66,19 @@ public class UserController {
         }
     }
 
+    @PutMapping("/editPreferencesByNames")
+    public ResponseEntity<?> editPreferences(
+            @RequestParam(value = "userId", required = true) long userId,
+            @RequestParam(value = "preferencesNames", required = true) String... preferencesNames) {
+        try {
+            UserResponse res = userService.editPreferences(userId, preferencesNames);
+            return ResponseEntity.ok(res);
+        } catch (IllegalArgumentException e) {
+            String errorMessage = e.getMessage();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+        }
+    }
+
     @GetMapping
     public ResponseEntity<UserListDto> getAll(
             @RequestParam(value = "pageNo", required = false, defaultValue = "0") int pageNo,
@@ -86,6 +99,13 @@ public class UserController {
         System.out.println(userService.getUserById(id));
         return ResponseEntity.ok(userService.getUserById(id));
     }
+    
+    @DeleteMapping("/{id}")
+    
+    public ResponseEntity<DeleteUserResponseDto> deleteUserById(@PathVariable long id) {
+
+        return ResponseEntity.ok(userService.deleteUserById(id));
+    }
 }
 
 
@@ -99,61 +119,3 @@ public class UserController {
 
 
 
-
-
-
-
-
-
-
-
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api/v1/users")
-//@CrossOrigin(origins={ "http://localhost:5173", "http://localhost:5174","http://localhost:3000"})
-//
-//public class UserController {
-//
-//    private final UserServiceImpl userService;
-//
-//    @PostMapping("/register")
-//    public ResponseEntity<UserResponse> registerUser(@Valid @RequestBody  UserRegistrationRequest request)
-//            throws Exception{
-//        var registeredUser = userService.registerUser(request);
-//        return ResponseEntity.ok(registeredUser);
-//    }
-//
-//    @PostMapping("/addPreferencesByNames")
-//    public ResponseEntity<?> setOrUpdatePreferences(
-//            @RequestParam(value = "userId", required = true) long userId,
-//            @RequestParam(value = "preferencesNames", required = true) String... preferencesNames) {
-//        try {
-//            UserResponse res = userService.setOrUpdatePreferences(userId, preferencesNames);
-//            return ResponseEntity.ok(res);
-//        } catch (IllegalArgumentException e) {
-//            String errorMessage = e.getMessage();
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
-//        }
-//    }
-//
-//    @GetMapping
-//    public ResponseEntity<UserListDto> getAll(
-//            @RequestParam(value = "pageNo", required = false, defaultValue = "0") int pageNo,
-//            @RequestParam(value = "pageSize", required = false, defaultValue = "10") int pageSize,
-//            @RequestParam(value = "sortDir", required = false, defaultValue = "asc") String sortDir,
-//            @RequestParam(value = "sortBy", required = false, defaultValue = "id") String... sortBy
-//
-//
-//    ) {
-//
-//        var res = userService.getAll(pageNo, pageSize, sortDir, sortBy);
-//
-//        return ResponseEntity.ok(res);
-//
-//    }
-//    @GetMapping(value = "/{id}")
-//    public ResponseEntity<UserResponse> getUserById(@PathVariable long id) {
-//        System.out.println(userService.getUserById(id));
-//        return ResponseEntity.ok(userService.getUserById(id));
-//    }
